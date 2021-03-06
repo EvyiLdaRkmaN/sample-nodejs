4782808c1cb796ee7b962c600fb065c5bd068d2c
Richard Yordy <evil.darkman@hotmail.com>
2021-03-05 21:26:53 -0600

    pagina no found

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.get('/', (req, res ) =>{
     res.send('Hola mundo');
 });
 
+//para toda url difrente de las establecidas.
+app.get('*',( req, res )=>{
+    res.sendFile(__dirname + '/public/404.html');
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
