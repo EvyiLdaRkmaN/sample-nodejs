dbb8696755f8b89d8affd53782c95e9c3d46f57c
Richard Yordy <evil.darkman@hotmail.com>
2021-03-05 18:37:03 -0600

    implement express

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
 //Recibir peticiones sin uso de express
 
-const http = require('http');
+// const http = require('http');
 
-http.createServer( ( req, res )=>{
-    res.write('hola mundo desde http');
-    res.end();
-} )
-.listen( 8081 );
+// http.createServer( ( req, res )=>{
+//     res.write('hola mundo desde http');
+//     res.end();
+// } )
+// .listen( 8081 );
 
-console.log('escuchando el puerto: 8081');;
+// console.log('escuchando el puerto: 8081');;
+
+const express = require('express');
+const app = express();
+const port = 8081;
+
+app.get('/', (req, res ) =>{
+    res.send('Hola mundo');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
